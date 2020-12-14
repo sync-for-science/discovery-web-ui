6a439449ca1d0b89f74e8767101a21753027140c
erwin foxtree <foxtree@users.noreply.github.com>
2020-12-14 10:38:58 -0500

    parser: babel-eslint (e.g.: static fields)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     'airbnb',
   ],
   ignorePatterns: ['config/webpack.config.js'],
-  // parser: 'babel-eslint',
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
