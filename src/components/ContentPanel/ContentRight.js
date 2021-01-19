e013c3c466e2a8729599a7c3403faf10da9aa99e
Gilbert Hsu <gilbert@vermonster.com>
2021-01-19 11:33:44 -0500

    update content-right class, modify mui card

--- src/components/ContentPanel/ContentRight.js
+++ src/components/ContentPanel/ContentRight.js
@@ -20,8 +20,8 @@ import Benefits from '../Benefits';
 import Claims from '../Claims';
 import Conditions from '../Conditions';
 import DocumentReferences from '../DocumentReferences';
-import Encounters from '../Encounters';
-// import Encounters from '../cards/Encounters'
+// import Encounters from '../Encounters';
+import Encounters from '../cards/Encounters'
 import Exams from '../Exams';
 import Immunizations from '../Immunizations';
 import LabResults from '../LabResults';
@@ -636,7 +636,7 @@ class ContentPanel extends React.PureComponent {
         </div>
       )];
     return (
-      <div className="content-panel-inner-body">
+      <div className="content-right-inner-body">
         { this.state.showJSON
           ? (
             <pre className="content-panel-data">
