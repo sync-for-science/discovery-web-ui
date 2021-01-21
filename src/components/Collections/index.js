48826012940a3bd37bf80f2906ed742c5b7e599f
Gilbert Hsu <gilbert@vermonster.com>
2021-01-21 12:31:07 -0500

    extract RecordCard, add mui to RecordCard

--- src/components/Collections/index.js
+++ src/components/Collections/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 import { connectToResources } from '../../recoil';
 import PersistentDrawerRight from '../ContentPanel/Drawer';
@@ -21,59 +16,14 @@ const useStyles = makeStyles({
   },
 });
 
-const RecordCard = ({ resource }) => {
-  const classes = useStyles();
-  const {
-    // provider,
-    // data,
-    data: {
-      // id,
-      resourceType,
-      // effectiveDateTime,
-      itemDate,
-    },
-  } = resource;
-
-  // if (id === 'f22de2b8-f549-416c-bea2-cdffe2585a65') {
-  //   console.error('id: ', id);
-  //   console.error('data: ', data);
-  // }
-
-  // const fields = Object.entries(data).map(([k, v]) => {
-  //   console.error(' k , v: ', k , v);
-  //   return (
-  //     <div>
-  //       { k }: { JSON.stringify(v) }
-  //     </div>
-  //   );
-  // });
-
-  return (
-    <Card
-      className={classes.root}
-      elevation={4}
-    >
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          { resourceType }
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          { itemDate }
-        </Typography>
-        <Typography variant="body2" component="p" />
-      </CardContent>
-      <CardActions>
-        <Button size="small">Annotate</Button>
-      </CardActions>
-    </Card>
-  );
-};
-
 const CardList = ({ normalized }) => {
   if (!normalized) {
     return null;
   }
-  return normalized.map((r) => (<RecordCard resource={r} key={r.data.id} />));
+
+  const slice = normalized.slice(2, 3)
+
+  return slice.map((r) => <RecordCard resource={r} />);
 };
 
 const Collections = (props) => {
