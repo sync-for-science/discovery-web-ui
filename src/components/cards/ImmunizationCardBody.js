a18ebd2506fef55893f487b1749256820ddd8d3e
Gilbert Hsu <gilbert@vermonster.com>
2021-01-22 18:20:16 -0500

    update ImmunizationCardBody

--- src/components/cards/ImmunizationCardBody.js
+++ src/components/cards/ImmunizationCardBody.js
@@ -1,30 +1,44 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  highlight: {
-    backgroundColor: '#d78c14', // add this color to theme
-  }
-}));
+import CardBodyField from './CardBodyField'
 
 const ImmunizationCardBody = ({fieldsData}) => {
-  const classes = useStyles()
   const givenDisplay = fieldsData.notGiven ? 'no' : 'yes'
   const primarySourceDisplay = fieldsData.primarySource ? 'yes' : 'no'
+  const reportedDisplay = fieldsData.reported ? 'yes' : 'no'
   return (
     <>
-      <Grid item xs={4}><Typography variant="body2">VACCINE</Typography></Grid>
-      <Grid item xs={8}><Typography variant="body2" className={classes.highlight}>{fieldsData.vaccineDisplay}</Typography></Grid>
-      <Grid item xs={4}><Typography variant="body2">GIVEN</Typography></Grid>
-      <Grid item xs={8}><Typography variant="body2">{givenDisplay}</Typography></Grid>
-      <Grid item xs={4}><Typography variant="body2">PRIMARY SOURCE</Typography></Grid>
-      <Grid item xs={8}><Typography variant="body2">{primarySourceDisplay}</Typography></Grid>
-      <Grid item xs={4}><Typography variant="body2">PROVIDER</Typography></Grid>
-      <Grid item xs={8}><Typography variant="body2">{fieldsData.provider}</Typography></Grid>
-      <Grid item xs={4}><Typography variant="body2">STATUS</Typography></Grid>
-      <Grid item xs={8}><Typography variant="body2">{fieldsData.status}</Typography></Grid>
+      <CardBodyField 
+        dependency={fieldsData.vaccineDisplay} 
+        label="VACCINE" 
+        value={fieldsData.vaccineDisplay} 
+        highlight
+      />
+      <CardBodyField 
+        dependency={fieldsData.notGiven !== undefined} 
+        label="GIVEN" 
+        value={givenDisplay} 
+      />
+      <CardBodyField 
+        dependency={fieldsData.reported} 
+        label="REPORTED" 
+        value={reportedDisplay} 
+      />
+      <CardBodyField 
+        dependency={fieldsData.primarySource} 
+        label="PRIMARY SOURCE" 
+        value={primarySourceDisplay} 
+      />
+      <CardBodyField 
+        dependency={fieldsData.provider} 
+        label="PROVIDER" 
+        value={fieldsData.provider} 
+      />
+      <CardBodyField 
+        dependency={fieldsData.status} 
+        label="STATUS" 
+        value={fieldsData.status} 
+      />
     </>
   )
 }
