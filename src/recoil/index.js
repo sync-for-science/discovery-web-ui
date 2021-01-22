e66d7dc2aea5e727b84c3839e6353ac7427e59c7
erwin foxtree <foxtree@users.noreply.github.com>
2021-01-22 09:35:57 -0500

    src/recoil/index.js

---
+++ src/recoil/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useRecoilValue, atom } from 'recoil';
+
+export const resourcesState = atom({
+  key: 'resourcesState', // unique ID (with respect to other atoms/selectors)
+  default: {
+    loading: false,
+    error: null,
+    raw: null,
+    normalized: null,
+    patient: null,
+    providers: [],
+    categories: [],
+    legacy: null,
+  },
+});
+
+export const filtersState = atom({
+  key: 'filtersState', // unique ID (with respect to other atoms/selectors)
+  default: {
+    dates: null,
+    thumbLeftDate: null,
+    thumbRightDate: null,
+  },
+});
+
+// read-only connection to resources and filters:
+export const connectToResources = (Component) => (props) => {
+  const resources = useRecoilValue(resourcesState);
+  const filters = useRecoilValue(filtersState);
+  // useContext(DiscoveryContext);
+
+  return (
+    <Component
+      {...props} // eslint-disable-line react/jsx-props-no-spreading
+      resources={resources}
+      filters={filters}
+    />
+  );
+};
