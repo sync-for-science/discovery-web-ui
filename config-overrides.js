e234af31b5a8521c3e1e9e0abda4b37ce6a9642a
erwin foxtree <foxtree@users.noreply.github.com>
2021-01-27 13:18:39 -0500

    config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,45 @@
-module.exports = (config) => {
-  require('react-app-rewire-postcss')(config, true); // eslint-disable-line global-require
+const rewirePostCSS = require('react-app-rewire-postcss');
+const {
+  // See:
+  // https://github.com/arackaf/customize-cra/blob/master/api.md
+  override,
+  // adjustStyleLoaders,
+  // addPostcssPlugins,
+  addBabelPlugins,
+  // addBabelPlugin,
+  // addExternalBabelPlugins,
+  // addExternalBabelPlugin,
+  // addDecoratorsLegacy,
+  // fixBabelImports,
+  // useBabelRc,
+} = require('customize-cra');
+// const postCssNested = require('postcss-nested');
+
+// Documentation for operators:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+
+// The following works, on its own:  (but without react-app-rewire-postcss / postcss-nested)
+// module.exports = override(
+//   // useBabelRc(),
+//   addBabelPlugins(
+//     '@babel/plugin-proposal-nullish-coalescing-operator',
+//     '@babel/plugin-proposal-optional-chaining',
+//   ),
+// );
+// ^ replace react-app-rewire-postcss with: addPostcssPlugins([postCssNested]) ?
+
+module.exports = (config, env) => {
+  override(
+    // useBabelRc(),
+    addBabelPlugins(
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+    ),
+    // addPostcssPlugins([postCssNested]),
+  )(config);
+
+  rewirePostCSS(config, true);
+
   return config;
 };
