1e6fd1e55ee90cf880f0fd631c02a51a38ec4d4c
erwin foxtree <foxtree@users.noreply.github.com>
2021-02-05 08:04:53 -0500

    disableGutters={true}

--- src/components/SelectedCardCollection/index.js
+++ src/components/SelectedCardCollection/index.js
@@ -58,6 +58,7 @@ const SelectedCardCollection = () => {
           .map(([displayCoding, uuids]) => (
             <Accordion
               key={displayCoding}
+              disableGutters
             >
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
