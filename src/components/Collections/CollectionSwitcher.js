7ab2b342dfc7d3122c8224bb31b893017a1d59f1
erwin foxtree <foxtree@users.noreply.github.com>
2021-02-08 17:36:41 -0500

    CollectionSwitcher: use MUI Select

--- src/components/Collections/CollectionSwitcher.js
+++ src/components/Collections/CollectionSwitcher.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import { useRecoilState } from 'recoil';
+
 import { allCollectionsState } from '../../recoil';
 
+const useStyles = makeStyles((_theme) => ({
+  formControl: {
+    width: '100%',
+  },
+}));
+
 const CollectionSwitcher = () => {
+  const classes = useStyles();
+
   const [allCollections, setAllCollections] = useRecoilState(allCollectionsState);
   const { activeCollectionId, collections } = allCollections;
-  const activeCollectionLabel = collections[activeCollectionId].label;
 
-  const setActiveCollection = (collectionId) => {
+  const handleChange = (event) => {
+    const { target: { value: collectionId } } = event;
     setAllCollections((previousState) => {
-      // console.error('handleAddNewCollection previousState: ', previousState);
       const { collections } = previousState;
       return {
         activeCollectionId: collectionId,
@@ -19,9 +32,34 @@ const CollectionSwitcher = () => {
   };
 
   return (
-    <div>
-      {activeCollectionLabel}
-    </div>
+    <FormControl
+      variant="outlined"
+      color="secondary"
+      className={classes.formControl}
+    >
+      <InputLabel
+        id="select-active-collection-label"
+      >
+        Active Collection
+      </InputLabel>
+      <Select
+        label="Active Collection"
+        labelId="select-active-collection-label"
+        id="select-active-collection"
+        value={activeCollectionId}
+        MenuProps={{ MenuListProps: { disablePadding: true } }}
+        onChange={handleChange}
+      >
+        {Object.entries(collections).map(([collectionId, { label }]) => (
+          <MenuItem
+            key={collectionId}
+            value={collectionId}
+          >
+            {label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 };
 
