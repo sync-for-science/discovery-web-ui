63996cf5d18a67fdd9d4c11e8f8791532fa63812
erwin foxtree <foxtree@users.noreply.github.com>
2021-02-11 11:11:18 -0500

    convert CompareView to pure functional component

--- src/components/CatalogView/index.js
+++ src/components/CatalogView/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { useRecoilValue } from 'recoil';
 
 import './CatalogView.css';
@@ -32,87 +31,59 @@ const NoResultsDisplay = React.memo(({ filteredRecordCount, activeCategories, ac
   );
 });
 
-class CompareView extends React.PureComponent {
-  state = {
-    firstTileColNum: 0,
-    numVisibleCols: 0,
-  }
-
-  renderTileColumns() {
-    const { filteredActiveCollection } = this.props;
-    const cols = Object.entries(filteredActiveCollection)
-      .sort(([categoryLabel1], [categoryLabel2]) => ((categoryLabel1 < categoryLabel2) ? -1 : 1))
-      .reduce((acc, [categoryLabel, category]) => {
-        if (category?.filteredRecordCount) {
-          acc.push(
-            <div className="tiles-view-column-container" key={categoryLabel}>
-              <div className="tiles-view-column-header">
-                {categoryLabel}
-                {/* <button className={this.buttonClass(categoryLabel)} onClick={() => this.handleSetClearButtonClick(categoryLabel)} /> */}
-              </div>
-              <div className="tiles-view-column-content">
-                { Object.entries(category.subtypes)
-                  .sort(([subtype1], [subtype2]) => ((subtype1 < subtype2) ? -1 : 1))
-                  .reduce((acc, [displayCoding, { uuids, _collectionUuids }]) => {
-                    if (uuids.length) {
-                      acc.push(<RecordSelector
-                        key={displayCoding}
-                        label={displayCoding}
-                        uuids={uuids}
-                      />);
-                    }
-                    return acc;
-                  }, []) }
-              </div>
-            </div>,
-          );
-        }
-        return acc;
-      }, []);
-
-    return cols;
-  }
-
-  render() {
-    const { activeCategories, activeProviders, filteredActiveCollection: { filteredRecordCount } } = this.props;
-    return (
-      <div className="tiles-view">
-        <div className="tiles-view-header" />
-        <div className="tiles-view-container">
-          <div className="tiles-view-container-inner">
-            { this.renderTileColumns() }
-            <NoResultsDisplay
-              filteredRecordCount={filteredRecordCount}
-              activeCategories={activeCategories}
-              activeProviders={activeProviders}
-            />
-          </div>
-        </div>
-        <SelectedCardCollection />
-      </div>
-    );
-  }
-}
-
-CompareView.propTypes = {
-  filteredActiveCollection: PropTypes.shape({}),
-  activeCategories: PropTypes.shape({}),
-  activeProviders: PropTypes.shape({}),
-};
-
-const CompareViewHOC = (props) => {
+const CompareView = () => {
   const filteredActiveCollection = useRecoilValue(filteredActiveCollectionState);
   const activeCategories = useRecoilValue(activeCategoriesState);
   const activeProviders = useRecoilValue(activeProvidersState);
 
+  const { filteredRecordCount } = filteredActiveCollection;
+
+  const columnsForCategories = Object.entries(filteredActiveCollection)
+    .sort(([categoryLabel1], [categoryLabel2]) => ((categoryLabel1 < categoryLabel2) ? -1 : 1))
+    .reduce((acc, [categoryLabel, category]) => {
+      if (category?.filteredRecordCount) {
+        acc.push(
+          <div className="tiles-view-column-container" key={categoryLabel}>
+            <div className="tiles-view-column-header">
+              {categoryLabel}
+              {/* <button className={this.buttonClass(categoryLabel)} onClick={() => this.handleSetClearButtonClick(categoryLabel)} /> */}
+            </div>
+            <div className="tiles-view-column-content">
+              { Object.entries(category.subtypes)
+                .sort(([subtype1], [subtype2]) => ((subtype1 < subtype2) ? -1 : 1))
+                .reduce((acc, [displayCoding, { uuids, _collectionUuids }]) => {
+                  if (uuids.length) {
+                    acc.push(<RecordSelector
+                      key={displayCoding}
+                      label={displayCoding}
+                      uuids={uuids}
+                    />);
+                  }
+                  return acc;
+                }, []) }
+            </div>
+          </div>,
+        );
+      }
+      return acc;
+    }, []);
+
   return (
-    <CompareView
-      {...props} // eslint-disable-line react/jsx-props-no-spreading
-      filteredActiveCollection={filteredActiveCollection}
-      activeCategories={activeCategories}
-      activeProviders={activeProviders}
-    />
+    <div className="tiles-view">
+      <div className="tiles-view-header" />
+      <div className="tiles-view-container">
+        <div className="tiles-view-container-inner">
+          { columnsForCategories }
+          <NoResultsDisplay
+            filteredRecordCount={filteredRecordCount}
+            activeCategories={activeCategories}
+            activeProviders={activeProviders}
+          />
+        </div>
+      </div>
+      <SelectedCardCollection />
+    </div>
   );
 };
 
-export default CompareViewHOC;
+export default CompareView;
