bbfc158f896cda2085fe06af3d7cac3f76f5bc03
Vadim Peretokin <vperetokin@gmail.com>
2021-02-16 13:47:36 +0100

    Add buttons

--- src/components/ColumnBrowser/index.js
+++ src/components/ColumnBrowser/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const ColumnBrowser = ({ children, columns }) => {
-  // console.log('columns.length: ', columns.length);
+  const innerView = useRef();
+
+  const onNavClick = (dir) => {
+    if (dir === 'left') {
+      // innerView.current.scroll
+    } else {
+    }
+  };
+
   return (
-    <div className="tiles-view-container-inner">
-      { columns }
-      { children }
-    </div>
+    <>
+      <div className="tiles-view-nav-left">
+        <button
+          className={true ? 'tiles-view-nav-left-button-on' : 'tiles-view-nav-left-button-off'}
+          onClick={() => onNavClick('left')}
+        />
+      </div>
+
+      <div className="tiles-view-container-inner" ref={innerView}>
+        { columns }
+        { children }
+      </div>
+
+      <div className="tiles-view-nav-right">
+        <button
+          className={true ? 'tiles-view-nav-right-button-on' : 'tiles-view-nav-right-button-off'}
+          onClick={() => onNavClick('left')}
+        />
+      </div>
+    </>
   );
 };
-
 export default ColumnBrowser;
