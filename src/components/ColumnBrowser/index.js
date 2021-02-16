bdb37c0fe48147d111670fc7b024301e4d8cb254
Vadim Peretokin <vperetokin@gmail.com>
2021-02-16 16:46:55 +0100

    Cleanup JS

--- src/components/ColumnBrowser/index.js
+++ src/components/ColumnBrowser/index.js
@@ -3,20 +3,16 @@ import React, { useRef, useState, useEffect } from 'react';
 const ColumnBrowser = ({ children, columns }) => {
   const innerView = useRef();
   const [canScrollLeft, setCanScrollLeft] = useState(false);
-  const [canScrollRight, setcanScrollRight] = useState(false);
-  const distance = 50;
+  const [canScrollRight, setCanScrollRight] = useState(false);
+  const distance = 150;
 
   const onNavClick = (dir) => {
-    if (dir === 'left') {
-      innerView.current.scrollBy({ left: -50, behavior: 'smooth' });
-    } else {
-      innerView.current.scrollBy({ left: 50, behavior: 'smooth' });
-    }
+    innerView.current.scrollBy({ left: dir === 'left' ? -distance : distance, behavior: 'smooth' });
   };
 
   const checkForScrollPosition = () => {
     setCanScrollLeft(innerView.current.scrollLeft > 0);
-    setcanScrollRight(innerView.current.scrollLeft !== innerView.current.scrollWidth - innerView.current.clientWidth);
+    setCanScrollRight(innerView.current.scrollLeft !== innerView.current.scrollWidth - innerView.current.clientWidth);
   };
 
   useEffect(() => {
@@ -41,13 +37,6 @@ const ColumnBrowser = ({ children, columns }) => {
       <div
         className="tiles-view-container-inner"
         ref={innerView}
-        onScroll={(e) => {
-          // Ignore scroll events on nested divs
-          // (this check won't be necessary in React 17)
-          if (e.target === e.currentTarget) {
-            console.log('scrolling');
-          }
-        }}
       >
         { columns }
         { children }
